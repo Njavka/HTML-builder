8343ed83a49b9565219631e556e8da548f79372f
Navka <navka@list.ru>
2025-01-19 23:09:00 +0300

    feat: add read file info from folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const fileSys = require('fs');
+const path = require('path');
+
+const secretFolderPath = path.join(__dirname, 'secret-folder');
+
+
+fileSys.readdir(secretFolderPath, { withFileTypes: true }, (err, files) => {
+    if (err) {
+        console.error('Error reading directory:', err);
+        return;
+    }
+
+    files.forEach(file => {
+        if (file.isFile()) {
+            const filePath = path.join(secretFolderPath, file.name);
+            const fileSize = fileSys.statSync(filePath).size;
+            const fileExtension = path.extname(file.name).slice(1);
+            const fileName = path.basename(file.name, path.extname(file.name));
+            const sizeInKB = (fileSize / 1024).toFixed(3);
+
+            console.log(`${fileName} | ${fileExtension} | ${sizeInKB}kB`);
+        }
+    });
+});
