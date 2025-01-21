ad2bd512168d1914384f4bc29f8b7e6c1bb76afe
Navka <navka@list.ru>
2025-01-21 17:43:10 +0300

    refactor: replace sync method of getting file stats for async

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -13,12 +13,18 @@ fileSys.readdir(secretFolderPath, { withFileTypes: true }, (err, files) => {
     files.forEach(file => {
         if (file.isFile()) {
             const filePath = path.join(secretFolderPath, file.name);
-            const fileSize = fileSys.statSync(filePath).size;
+            fileSys.stat(filePath, (err, stats) => {
+              if (err) {
+                  console.error('Error getting file stats:', err);
+                  return;
+              }
+            const fileSize = stats.size;
             const fileExtension = path.extname(file.name).slice(1);
             const fileName = path.basename(file.name, path.extname(file.name));
             const sizeInKB = (fileSize / 1024).toFixed(3);
 
             console.log(`${fileName} - ${fileExtension} - ${sizeInKB}kB`);
+            });
         }
     });
 });
